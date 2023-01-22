b5a8f4ca564e5ebdfc75147eb85918c1ae248cca
JordanBScott <99845881+JordanBScott@users.noreply.github.com>
2023-01-21 21:17:20 -0500

    added discord status bot thing

--- config.lua
+++ config.lua
@@ -38,22 +38,22 @@ Config.EnableWantedLevel = false
 -- To Set This Up visit https://forum.cfx.re/t/how-to-updated-discord-rich-presence-custom-image/157686
 Config.Discord = {
     ["IsEnabled"] = true, -- If set to true, then discord rich presence will be enabled
-    ["ApplicationId"] = '915674222628585472', -- The discord application id
-    ["IconLarge"] = 'logo_name', -- The name of the large icon
-    ["IconLargeHoverText"] = 'This is a Large icon with text', -- The hover text of the large icon
-    ["IconSmall"] = 'small_logo_name', -- The name of the small icon
-    ["IconSmallHoverText"] = 'This is a Small icon with text', -- The hover text of the small icon
+    ["ApplicationId"] = '1053825704216768523', -- The discord application id
+    ["IconLarge"] = 'cprp512x512', -- The name of the large icon
+    ["IconLargeHoverText"] = 'Criminals Paradise RP', -- The hover text of the large icon
+    ["IconSmall"] = 'cprp512x512', -- The name of the small icon
+    ["IconSmallHoverText"] = 'Criminals Paradise RP', -- The hover text of the small icon
     ["UpdateRate"] = 60000, -- How often the player count should be updated
     ["ShowPlayerCount"] = true, -- If set to true the player count will be displayed in the rich presence
-    ["MaxPlayers"] = 48, -- Maximum amount of players
+    ["MaxPlayers"] = 64, -- Maximum amount of players
     ["Buttons"] = {
         {
-            text = 'First Button!',
-            url = 'fivem://connect/localhost:30120'
+            text = 'CPRP Discord',
+            url = 'https://discord.gg/nnvz7aV8c4'
         },
         {
-            text = 'Second Button!',
-            url = 'fivem://connect/localhost:30120'
+            text = 'Join the Server Now!',
+            url = 'https://cfx.re/join/3rvdjz'
         }
     }
 }
